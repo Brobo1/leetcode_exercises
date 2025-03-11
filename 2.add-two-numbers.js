5cfd620b9f312cbdf28db82224ee6a09b1f78f66
Brobo1 <armanahz95@gmail.com>
2025-03-11 21:34:45 +0100

    `feat: update addTwoNumbers to use linked list instead of array`

--- 2.add-two-numbers.js
+++ 2.add-two-numbers.js
@@ -17,20 +17,28 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-let addTwoNumbers = function(l1, l2) {
-	let list = []
-	let s1 = ""
-	let s2 = ""
-	while (l1.length || l2.length) {
-		s1 += l1.pop()
-		s2 += l2.pop()
-	}
-	let total = (parseInt(s1) + parseInt(s2)).toString().split("")
-	while (total.length) {
-		list.push(parseInt(total.pop()))
-	}
-	return list
+let addTwoNumbers = function (l1, l2) {
+  let list = []
+  let s1 = ""
+  let s2 = ""
+  while (l1 || l2) {
+    s1 += l1.pop()
+    s2 += l2.pop()
+  }
+  let total = (parseInt(s1) + parseInt(s2)).toString().split("")
+  
+  let head = new ListNode(parseInt(total.pop()))
+  let current = head
+  while (total.length) {
+    // list.push(parseInt(total.pop()))
+    current.next = new ListNode(parseInt(total.pop()))
+    current = current.next
+  }
+  return head
 };
 
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
 // @lc code=end
-
