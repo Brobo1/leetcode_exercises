823e8a82b60a2d376f2e79db08d367e99d5979f4
Brobo1 <armanahz95@gmail.com>
2025-03-11 21:51:53 +0100

    refactor: remove unused ListNode constructor for cleaner code

--- 2.add-two-numbers.js
+++ 2.add-two-numbers.js
@@ -43,9 +43,5 @@ let addTwoNumbers = function (l1, l2) {
   return head.next;
 };
 
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
 
 // @lc code=end
