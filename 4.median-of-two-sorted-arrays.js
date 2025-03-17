35a5b1a4d59c247662e63a34ba2266403387e5db
Brobo1 <armanahz95@gmail.com>
2025-03-17 22:46:41 +0100

    feat: implement merging logic in findMedianSortedArrays function

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -13,8 +13,17 @@
 var findMedianSortedArrays = function(nums1, nums2) {
     const merged = []
 		let i = 0;
-		let j = 0; 
+		let j = 0;
 
+		while(i < nums1.length && j < nums2.length) {
+			if (nums1[i] > num2[j]) {
+				merged.push(nums1[i])
+				i++
+			} else {
+				merged.push(nums2[j])
+				j++
+			}
+		}
 };
 // @lc code=end
 
