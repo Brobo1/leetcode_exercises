565c30e733a90d3504665cadc6f592ae529db2c9
Brobo1 <armanahz95@gmail.com>
2025-03-17 22:57:54 +0100

    feat: update merging logic in findMedianSortedArrays to handle equal elements and add console logging

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -10,20 +10,35 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-    const merged = []
-		let i = 0;
-		let j = 0;
 
-		while(i < nums1.length && j < nums2.length) {
-			if (nums1[i] > num2[j]) {
-				merged.push(nums1[i])
-				i++
-			} else {
-				merged.push(nums2[j])
-				j++
-			}
+var findMedianSortedArrays = function(nums1, nums2) {
+	const merged = []
+	let i = 0;
+	let j = 0;
+
+	while(i < nums1.length && j < nums2.length) {
+		if (nums1[i] <= nums2[j]) {
+			merged.push(nums1[i])
+			i++
+		} else {
+			merged.push(nums2[j])
+			j++ 
 		}
+	}
+
+	while(i < nums1.length) {
+		merged.push(nums1[i])
+		i++
+	}
+	while(j < nums2.length) {
+		merged.push(nums2[j])
+		j++
+	}
+
+	console.log(merged);
+		
 };
+
+findMedianSortedArrays([1,3], [2])
 // @lc code=end
 
